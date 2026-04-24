2beea7cf3b9b88bcde21ee8a0cc3405db166821e
user <igorechekneznaika@gmail.com>
2026-04-24 19:08:56 +0200

    first_commit

---
+++ android/src/ru/innovationcampus/vsu26/igrekon/space_cleaner/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.innovationcampus.vsu26.igrekon.space_cleaner;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.innovationcampus.vsu26.igrekon.space_cleaner.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
